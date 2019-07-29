cf3a139ecf2e4b1e6794f209b9b941af0198e4ee
lyricporter2019 <52715530+lyricporter2019@users.noreply.github.com>
2019-07-29 10:08:28 -0500

    Add files via upload

---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by OH Apple One Summer Chicago on 7/25/19.
+//  Copyright © 2019 OH Apple One Summer Chicago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
